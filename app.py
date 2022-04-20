74f2221ed49b677594277ca76e5aebd1931451e1
Javier Sabín Ulloa <99047531+jsabin408204@users.noreply.github.com>
2022-04-20 21:11:47 +0200

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 from PIL import Image
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
 st.title('My first web app')
-image = Image.open("C:/Users/xaver/OneDrive/Imágenes/Logo-KDT-JU.jpg")
+image = Image.open("C:/Users/xaver/OneDrive/Imágenes/Logo-KDT-JU.jpg", mode = 'r')
 st.image(image)
 st.write(chart_data)
 option = st.selectbox('Select an option:', ['a','b','c'])
