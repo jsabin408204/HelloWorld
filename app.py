e05acd6823f531ecdf423da72dea1492cc140666
Javier Sabín Ulloa <99047531+jsabin408204@users.noreply.github.com>
2022-04-20 21:15:39 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,8 +6,7 @@ from pathlib import Path
 
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
 st.title('My first web app')
-image = Path("C:/Users/xaver/OneDrive/Imágenes/Logo-KDT-JU.jpg")
-image = Image.open(image, mode = 'r')
+image = open("C:\Users\xaver\OneDrive\Imágenes\Logo-KDT-JU.jpg", "r")
 st.image(image)
 st.write(chart_data)
 option = st.selectbox('Select an option:', ['a','b','c'])
