9da159541650539184ce8778ad96c290ebf5a318
Javier Sabín Ulloa <99047531+jsabin408204@users.noreply.github.com>
2022-04-20 21:03:53 +0200

    Add files via upload

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
 st.title('My first web app')
 st.image("C:/Users/xaver/OneDrive/Imágenes/Logo-KDT-JU.jpg")
-
-chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
 st.write(chart_data)
 option = st.selectbox(['a', 'b', 'c'])
 st.bar_chart(chart_data[option])
