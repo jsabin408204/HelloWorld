eb729c21fad5baa0e86b62d4679c6895d08d77ce
Javier Sabín Ulloa <99047531+jsabin408204@users.noreply.github.com>
2022-04-20 21:10:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-
+from PIL import Image
 chart_data = pd.DataFrame(np.random.randn(20, 3), columns=['a', 'b', 'c'])
 st.title('My first web app')
-st.image("C:/Users/xaver/OneDrive/Imágenes/Logo-KDT-JU.jpg")
+image = Image.open("C:/Users/xaver/OneDrive/Imágenes/Logo-KDT-JU.jpg")
+st.image(image)
 st.write(chart_data)
 option = st.selectbox('Select an option:', ['a','b','c'])
 st.bar_chart(chart_data['a'])
