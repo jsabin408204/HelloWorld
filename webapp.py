4ee0e6fe26a47814d2485675679c30f207f34bf6
Javier Sabín Ulloa <99047531+jsabin408204@users.noreply.github.com>
2022-04-22 21:05:58 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -17,7 +17,7 @@ option = st.selectbox('Country:', custom_participants['Country'].unique())
 
 custom_participants=pd.read_sql('''SELECT projects.year, SUM(ecContribution) AS grants, countries.Country
 FROM participants, projects, countries
-WHERE participants.projectID == projects.projectID AND participants.country == countries.acronym AND participants.country == '{}'
+WHERE participants.projectID == projects.projectID AND participants.country == countries.acronym AND countries.Country == '{}' 
 GROUP BY projects.year'''.format(option) , connection)
 
 # Creating a plot of the overall aggregated contribution per year. This will allow us to see if we approached the problem correctly, and then proceed with the view per country.
