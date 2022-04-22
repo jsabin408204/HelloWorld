725b78244a7409ce106250002d6a1be6f0b639ed
Javier Sabín Ulloa <99047531+jsabin408204@users.noreply.github.com>
2022-04-22 21:04:26 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -13,7 +13,7 @@ FROM participants, projects, countries
 WHERE participants.projectID == projects.projectID AND participants.country == countries.acronym 
 GROUP BY projects.year''', connection)
 
-option = st.selectbox('Country:', custom_participants['Country'])
+option = st.selectbox('Country:', custom_participants['Country'].unique())
 
 custom_participants=pd.read_sql('''SELECT projects.year, SUM(ecContribution) AS grants, countries.Country
 FROM participants, projects, countries
