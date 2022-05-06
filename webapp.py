4b12b9d0359a277eac016c13f864d0989cf94057
Javier Sabín Ulloa <99047531+jsabin408204@users.noreply.github.com>
2022-05-06 19:15:21 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -24,4 +24,5 @@ FROM participants, projects, countries
 WHERE participants.projectID == projects.projectID AND participants.country == countries.acronym AND countries.Country == '{}' 
 GROUP BY projects.year'''.format(option) , connection)
 
+st.header('Participants in {}'.format(option))
 st.write(best)
