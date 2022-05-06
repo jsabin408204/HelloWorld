95b3058bf02ae0e474f0f3f3b6023ba4102d20a5
Javier Sabín Ulloa <99047531+jsabin408204@users.noreply.github.com>
2022-05-06 19:17:56 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -10,7 +10,7 @@ st.title('Partner search tool')
 
 option = st.selectbox('Country:', dropcountries["Country"].unique())
 
-custom_participants=pd.read_sql('''SELECT projects.year, SUM(ecContribution) AS grants, countries.Country
+custom_participants=pd.read_sql('''SELECT projects.year, SUM(ecContribution) AS Grants, countries.Country
 FROM participants, projects, countries
 WHERE participants.projectID == projects.projectID AND participants.country == countries.acronym AND countries.Country == '{}' 
 GROUP BY projects.year'''.format(option) , connection)
