56a0622325801b758f8e3f31af2af94ea4f19a7b
Javier Sabín Ulloa <99047531+jsabin408204@users.noreply.github.com>
2022-05-06 19:14:19 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -16,7 +16,7 @@ WHERE participants.projectID == projects.projectID AND participants.country == c
 GROUP BY projects.year'''.format(option) , connection)
 
 # Creating a plot of the overall aggregated contribution per year. This will allow us to see if we approached the problem correctly, and then proceed with the view per country.
-st.header('Yearly EC contribution in Spain (€)')
+st.header('Yearly EC contribution in {} (€)'.format(option))
 st.bar_chart(custom_participants['grants'])
 
 best=pd.read_sql('''SELECT participants.shortName, participants.name, participants.activityType, participants.organizationURL 
