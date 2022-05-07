60a370709bf81018832444b0400c8f34b2d04137
Javier Sabín Ulloa <99047531+jsabin408204@users.noreply.github.com>
2022-05-07 12:23:08 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -1,46 +1,51 @@
+# Importing the required libraries and modules
 import streamlit as st
 import pandas as pd
 import sqlite3
 
-connection=sqlite3.connect("partnersearchapp.sqlite") #We are connecting to the database.
+# Connecting to the database
+connection=sqlite3.connect("partnersearchapp.sqlite") 
 
-dropcountries=pd.read_sql('''SELECT Country FROM countries''',connection)
-
+# Image at the top of the webapp
 st.image('KDT logo.jpg')
 
+# Title of the webapp
 st.title('Partner search tool')
 
-# Creating a dataframe to ensure only the existing countries are shown in the select box
+# Dataframe to ensure only the existing countries in participants are shown in the select box
 checked_countries = pd.read_sql('''SELECT countries.Country AS Country
 FROM participants, countries
 WHERE participants.country == countries.acronym''', connection)
 
+# Saving the selected country in the select box
 option = st.selectbox('Country:', checked_countries['Country'].unique())
 
+# Creating the dataframe of participants of the selected country grouped by project year and in descending order of contribution
 custom_participants=pd.read_sql('''SELECT participants.shortName, participants.name, participants.activityType, participants.organizationURL, COUNT(participants.projectID) as count_project, SUM(ecContribution) as sum_ecContribution, projects.year as year
 FROM participants, projects, countries
 WHERE participants.projectID == projects.projectID AND participants.country == countries.acronym AND countries.Country == '{}' 
 GROUP BY projects.year
 ORDER BY sum_ecContribution DESC'''.format(option) , connection, index_col = 'year')
 
-# Creating a plot of the overall aggregated contribution per year. This will allow us to see if we approached the problem correctly, and then proceed with the view per country.
+# Creating a plot of the contribution per year of a given country
 st.header('Yearly EC contribution in {} (€)'.format(option))
 st.bar_chart(custom_participants['sum_ecContribution'])
 
+# Printing the participants' dataframe
 st.header('Participants in {}'.format(option))
 st.write(custom_participants)
 
-st.header('Coordinators in {}'.format(option))
-
+# Creating the dataframe of coordinators of the selected country in ascending order by shortName
 coordinators=pd.read_sql('''SELECT participants.shortName, participants.name, participants.activityType, projects.acronym
 FROM participants, projects, countries
 WHERE participants.projectID == projects.projectID AND participants.country == countries.acronym AND countries.Country == '{}' AND participants.role == "coordinator"
 ORDER BY shortName'''.format(option) , connection)
+
+# Printing the coordinators' dataframe
+st.header('Coordinators in {}'.format(option))
 st.write(coordinators)
 
-def convert_df(df):
- return df.to_csv().encode('utf-8')
-
-st.download_button(label="Download the participants' dataset",data=convert_df(custom_participants), file_name='participants.csv', mime='text/csv')
-st.download_button(label="Download the coordinators' dataset",data=convert_df(coordinators), file_name='coordinators.csv', mime='text/csv')
+# Download buttons for the participants' and coordinators' datasets in csv format
+st.download_button(label="Download the participants' dataset",data=custom_participants.to_csv().encode('utf-8'), file_name='participants.csv', mime='text/csv')
+st.download_button(label="Download the coordinators' dataset",data=coordinators.to_csv().encode('utf-8'), file_name='coordinators.csv', mime='text/csv')
 
