82dbd926ee39153728c0b328d66432e4a8c13c87
Javier Sabín Ulloa <99047531+jsabin408204@users.noreply.github.com>
2022-05-07 12:33:57 +0200

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -46,7 +46,7 @@ ORDER BY shortName'''.format(option) , connection)
 
 # Printing the coordinators' dataframe with a condition in case it is empty
 if len(coordinators.index) == 0:
-  st.header('No project coordinators available in {}'.format(option))
+  st.text('No project coordinators available in {}'.format(option))
 else:
   st.header('Coordinators in {}'.format(option))
   st.write(coordinators)
