751d89c5704754c2340808a43019a603e2862a89
Javier Sabín Ulloa <99047531+jsabin408204@users.noreply.github.com>
2022-05-11 21:58:07 +0200

    Update final_enhanced.py

--- final_enhanced.py
+++ final_enhanced.py
@@ -45,7 +45,7 @@ st.download_button(label="Download the participants' dataset",data=custom_partic
 # Creating the dataframe of coordinators of the selected country in ascending order by shortName
 coordinators=pd.read_sql('''SELECT participants.shortName, participants.name, participants.activityType, projects.acronym
 FROM participants, projects, countries
-WHERE participants.projectID == projects.projectID AND participants.country == countries.acronym AND countries.Country == '{}' AND participants.role == "coordinator"
+WHERE participants.projectID == projects.projectID AND participants.country == countries.acronym AND countries.Country == '{}' AND participants.role == "coordinator" 
 ORDER BY shortName'''.format(country_option) , connection)
 
 # Printing the coordinators' dataframe with a condition in case it is empty for a given country
