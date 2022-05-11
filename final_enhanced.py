1fea9f711ac6aee90732fe306d5121d3b9bcee5d
Javier Sabín Ulloa <99047531+jsabin408204@users.noreply.github.com>
2022-05-12 00:50:06 +0200

    Update final_enhanced.py

--- final_enhanced.py
+++ final_enhanced.py
@@ -39,25 +39,25 @@ st.header('Yearly EC contribution in {} (€)'.format(country_option))
 st.bar_chart(custom_participants['sum_ecContribution'])
 
 # Printing the participants' dataframe
-st.header('Participants in {}'.format(country_option))
+st.header('Participants in {} in {}'.format(country_option, year_option))
 st.write(custom_participants)
 
 # Download button for the participants' dataset in csv format
-st.download_button(label="Download the participants' dataset",data=custom_participants.to_csv().encode('utf-8'), file_name='Participants in {}.csv'.format(country_option), mime='text/csv')
+st.download_button(label="Download the participants' dataset",data=custom_participants.to_csv().encode('utf-8'), file_name='Participants in {} in {}.csv'.format(country_option, year_option), mime='text/csv')
 
 # Creating the dataframe of coordinators of the selected country in ascending order by shortName
 coordinators=pd.read_sql('''SELECT participants.shortName, participants.name, participants.activityType, projects.acronym
 FROM participants, projects, countries
-WHERE participants.projectID == projects.projectID AND participants.country == countries.acronym AND countries.Country == '{}' AND participants.role == "coordinator" 
-ORDER BY shortName'''.format(country_option) , connection)
+WHERE participants.projectID == projects.projectID AND participants.country == countries.acronym AND countries.Country == '{}' AND participants.role == "coordinator" AND projects.year == '{}'
+ORDER BY shortName'''.format(country_option, year_option) , connection)
 
 # Printing the coordinators' dataframe with a condition in case it is empty for a given country
-st.header('Project coordinators in {}'.format(country_option))
+st.header('Project coordinators in {} in {}'.format(country_option, year_option))
 if len(coordinators.index) == 0:
-  st.write('No project coordinators available in {}'.format(country_option))
+  st.write('No project coordinators available in {} in {}'.format(country_option, year_option))
 else:
   st.write(coordinators)
   # Download button for the coordinators' dataset in csv format
-  st.download_button(label="Download the coordinators' dataset",data=coordinators.to_csv().encode('utf-8'), file_name='Project coordinators in {}.csv'.format(country_option), mime='text/csv')
+  st.download_button(label="Download the coordinators' dataset",data=coordinators.to_csv().encode('utf-8'), file_name='Project coordinators in {} in {}.csv'.format(country_option, year_option), mime='text/csv')
 
 a = st.sidebar.radio('Select one:', [1, 2])
