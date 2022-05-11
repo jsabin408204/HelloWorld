02ae8ae8f87d44fe2bc55c366c219b91d51d55e8
Javier Sabín Ulloa <99047531+jsabin408204@users.noreply.github.com>
2022-05-11 22:09:49 +0200

    Update final_enhanced.py

--- final_enhanced.py
+++ final_enhanced.py
@@ -22,7 +22,11 @@ country_option = st.selectbox('Country:', checked_countries['Country'].unique())
 acronym_option = checked_countries[checked_countries['Country'] == country_option].Acronym.sample().item()
 st.write('You selected {}-{}'.format(acronym_option, country_option))
 
-year = st.slider('How old are you?', 0, 130, 25)
+checked_countries = pd.read_sql('''SELECT countries.Country AS Country, countries.Acronym AS Acronym, participants.year as Year
+FROM participants, countries
+WHERE participants.country == countries.acronym''', connection)
+
+year = st.slider('What year would you like to see?', 0, 130, 25)
 
 # Creating the dataframe of participants of the selected country grouped by project year and in descending order of contribution
 custom_participants=pd.read_sql('''SELECT participants.shortName, participants.name, participants.activityType, participants.organizationURL, COUNT(participants.projectID) as count_project, SUM(ecContribution) as sum_ecContribution, projects.year as year
