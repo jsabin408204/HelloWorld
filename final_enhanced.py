96274b310751e173cd70b5645e466904d2e921d2
Javier Sabín Ulloa <99047531+jsabin408204@users.noreply.github.com>
2022-05-12 11:14:44 +0200

    Update final_enhanced.py

--- final_enhanced.py
+++ final_enhanced.py
@@ -55,10 +55,10 @@ st.bar_chart(custom_participants['sum_ecContribution'])
 if year_preference == 'All years':
   st.write('Change between two years')
   available_years = custom_participants.index.tolist()
-  first_year = st.radio('First year:', available_years)
+  first_year = st.sidebar.radio('First year:', available_years)
   available_years_after = [year for year in available_years if year > first_year]
   if len(available_years_after) > 0:
-    second_year = st.radio('Second year:', available_years_after)
+    second_year = st.sidebar.radio('Second year:', available_years_after)
     difference = custom_participants.loc[second_year, 'sum_ecContribution'] - custom_participants.loc[first_year, 'sum_ecContribution']
     st.metric('Overall difference in aggregated grants', custom_participants.loc[second_year, 'sum_ecContribution'],difference)
   else:
