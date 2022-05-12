e86c3020f18116110d9e50c612005342828c456a
Javier Sabín Ulloa <99047531+jsabin408204@users.noreply.github.com>
2022-05-12 10:55:08 +0200

    Update final_enhanced.py

--- final_enhanced.py
+++ final_enhanced.py
@@ -49,13 +49,13 @@ else:
   WHERE participants.projectID == projects.projectID AND participants.country == countries.acronym AND countries.Country == '{}' AND participants.role == "coordinator"
   ORDER BY shortName'''.format(country_option), connection, index_col = 'year')
   available_years = custom_participants.index.tolist()
-  difference = custom_participants.loc[max(available_year), 'sum_ecContribution'] - custom_participants.loc[min(available_year), 'sum_ecContribution']
+  difference = custom_participants.loc[max(available_years), 'sum_ecContribution'] - custom_participants.loc[min(available_years), 'sum_ecContribution']
 
 # Creating a plot of the contribution per year of a given country
 st.header('Yearly EC contribution in {} (€)'.format(country_option))
 st.bar_chart(custom_participants['sum_ecContribution'])
 if year_preference == 'All years':
-  st.metric('Overall change in aggregated grants', custom_participants.loc[max(available_year), 'sum_ecContribution'],1)
+  st.metric('Overall change in aggregated grants', custom_participants.loc[max(available_years), 'sum_ecContribution'],difference)
 
 # Printing the participants' dataframe
 st.header('Participants in {}'.format(country_option))
