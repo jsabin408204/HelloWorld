bc915f0d82b2bb1c11e6185fb26fedd232222b83
Javier Sabín Ulloa <99047531+jsabin408204@users.noreply.github.com>
2022-05-12 10:33:47 +0200

    Update final_enhanced.py

--- final_enhanced.py
+++ final_enhanced.py
@@ -55,7 +55,7 @@ else:
 st.header('Yearly EC contribution in {} (€)'.format(country_option))
 st.bar_chart(custom_participants['sum_ecContribution'])
 if year_preference == 'All years':
-  st.metric('Overall change in aggregated grants', custom_participants.loc[max(available_year), 1)
+  st.metric('Overall change in aggregated grants', custom_participants.loc[max(available_year), 'sum_ecContribution'],1)
 
 # Printing the participants' dataframe
 st.header('Participants in {}'.format(country_option))
