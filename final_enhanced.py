ca117d3620f5b52cf7288f4d8f222cd0a504efcc
Javier Sabín Ulloa <99047531+jsabin408204@users.noreply.github.com>
2022-05-12 13:45:01 +0200

    Update final_enhanced.py

--- final_enhanced.py
+++ final_enhanced.py
@@ -20,7 +20,7 @@ WHERE participants.country == countries.acronym''', connection)
 
 # Saving the selected country from the select box, generating its acronym and printing the chosen country with its acronym
 unique_countries = checked_countries['Country'].unique().tolist()
-country_option = st.selectbox('Country:', unique_countries)
+country_option = st.selectbox('Country:', unique_countries.sort())
 acronym_option = checked_countries[checked_countries['Country'] == country_option].Acronym.sample().item()
 st.write('You selected {}-{}'.format(acronym_option, country_option))
 
