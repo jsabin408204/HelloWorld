e8dea1cc0d10483acd686604ebdd79006d23f5c1
Javier Sabín Ulloa <99047531+jsabin408204@users.noreply.github.com>
2022-05-12 09:26:06 +0200

    Update final.py

--- final.py
+++ final.py
@@ -41,10 +41,10 @@ st.write(custom_participants)
 st.download_button(label="Download the participants' dataset",data=custom_participants.to_csv().encode('utf-8'), file_name='Participants in {}.csv'.format(country_option), mime='text/csv')
 
 # Creating the dataframe of coordinators of the selected country in ascending order by shortName
-coordinators=pd.read_sql('''SELECT participants.shortName, participants.name, participants.activityType, projects.acronym
+coordinators=pd.read_sql('''SELECT participants.shortName, participants.name, participants.activityType, projects.acronym, projects.year AS year
 FROM participants, projects, countries
 WHERE participants.projectID == projects.projectID AND participants.country == countries.acronym AND countries.Country == '{}' AND participants.role == "coordinator"
-ORDER BY shortName'''.format(country_option) , connection)
+ORDER BY shortName'''.format(country_option), connection, index_col = 'year')
 
 # Printing the coordinators' dataframe with a condition in case it is empty for a given country
 st.header('Project coordinators in {}'.format(country_option))
