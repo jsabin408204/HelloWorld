f2d1c8bb7277adabd5ac1b1b531bf6320bdf45ca
Javier Sabín Ulloa <99047531+jsabin408204@users.noreply.github.com>
2022-05-13 09:47:57 +0200

    Update final_enhanced.py

--- final_enhanced.py
+++ final_enhanced.py
@@ -38,7 +38,7 @@ if year_preference == 'Specific year':
   WHERE participants.projectID == projects.projectID AND participants.country == countries.acronym AND countries.Country == '{}' AND projects.year == '{}'
   GROUP BY projects.year
   ORDER BY sum_ecContribution DESC'''.format(country_option, year_option), connection, index_col = 'year')
-  coordinators=pd.read_sql('''SELECT participants.shortName, participants.name, participants.activityType, projects.acronym, projects.year AS year
+  coordinators=pd.read_sql('''SELECT participants.shortName, participants.name, participants.activityType, projects.acronym AS projectAcronym, projects.year AS year
   FROM participants, projects, countries
   WHERE participants.projectID == projects.projectID AND participants.country == countries.acronym AND countries.Country == '{}' AND participants.role == "coordinator" AND projects.year == '{}'
   ORDER BY shortName'''.format(country_option, year_option), connection, index_col = 'year')
@@ -49,7 +49,7 @@ else:
   WHERE participants.projectID == projects.projectID AND participants.country == countries.acronym AND countries.Country == '{}'
   GROUP BY projects.year
   ORDER BY sum_ecContribution DESC'''.format(country_option), connection, index_col = 'year')
-  coordinators=pd.read_sql('''SELECT participants.shortName, participants.name, participants.activityType, projects.acronym, projects.year AS year
+  coordinators=pd.read_sql('''SELECT participants.shortName, participants.name, participants.activityType, projects.acronym AS projectAcronym, projects.year AS year
   FROM participants, projects, countries
   WHERE participants.projectID == projects.projectID AND participants.country == countries.acronym AND countries.Country == '{}' AND participants.role == "coordinator"
   ORDER BY shortName'''.format(country_option), connection, index_col = 'year')
